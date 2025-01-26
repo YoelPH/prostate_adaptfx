fix: fixed the full plan calculations changed the full plan calculation such that it now properly calculates the fractionation even if a number different from 5 is inserted.

adaptive_fractionation_overlap/core_adaptfx.py
@@ -160,7 +160,7 @@ def adaptive_fractionation_core(fraction: int, volumes: np.ndarray, accumulated_
     return [policies, policies_overlap, volume_space, physical_dose, penalty_added, values, dose_space, probabilities, final_penalty]
     
    
-def adaptfx_full(volumes: list, number_of_fractions: float = 5, min_dose: float = 7.5, max_dose: float = 9.5, mean_dose: float = 8, dose_steps: float = 0.25, alpha: float = 0.8909285040669036, beta:float = 0.522458969735114):
+def adaptfx_full(volumes: list, number_of_fractions: int = 5, min_dose: float = 7.5, max_dose: float = 9.5, mean_dose: float = 8, dose_steps: float = 0.25, alpha: float = 0.8909285040669036, beta:float = 0.522458969735114):
     """Computes a full adaptive fractionation plan when all overlap volumes are given.
 
     Args:
@@ -175,15 +175,14 @@ def adaptfx_full(volumes: list, number_of_fractions: float = 5, min_dose: float 
         accumullated_doses (array with the accumulated dose in each fraction),
         total_penalty (final penalty after fractionation if all suggested doses are applied)
     """
-    
     physical_doses = np.zeros(number_of_fractions)
     accumulated_doses = np.zeros(number_of_fractions)
     for index, frac in enumerate(range(1,number_of_fractions +1)):
         if frac != number_of_fractions:
-            [policies, policies_overlap, volume_space, physical_dose, penalty_added, values, dose_space, probabilities, final_penalty]  = adaptive_fractionation_core(fraction = frac,volumes = volumes[:-number_of_fractions+frac], accumulated_dose = accumulated_doses[index], min_dose = min_dose, max_dose = max_dose, mean_dose = mean_dose, dose_steps = dose_steps, alpha = alpha, beta = beta)
+            [policies, policies_overlap, volume_space, physical_dose, penalty_added, values, dose_space, probabilities, final_penalty]  = adaptive_fractionation_core(fraction = frac, volumes = volumes[:-number_of_fractions+frac], accumulated_dose = accumulated_doses[index], number_of_fractions= number_of_fractions, min_dose = min_dose, max_dose = max_dose, mean_dose = mean_dose, dose_steps = dose_steps, alpha = alpha, beta = beta)
             accumulated_doses[index+1] = accumulated_doses[index] + physical_dose
         else:
-            [policies, policies_overlap, volume_space, physical_dose, penalty_added, values, dose_space, probabilities, final_penalty]  = adaptive_fractionation_core(fraction = frac, volumes = volumes,accumulated_dose = accumulated_doses[index], min_dose = min_dose, max_dose = max_dose, mean_dose = mean_dose, dose_steps = dose_steps, alpha = alpha, beta = beta)
+            [policies, policies_overlap, volume_space, physical_dose, penalty_added, values, dose_space, probabilities, final_penalty]  = adaptive_fractionation_core(fraction = frac, volumes = volumes,accumulated_dose = accumulated_doses[index], number_of_fractions= number_of_fractions, min_dose = min_dose, max_dose = max_dose, mean_dose = mean_dose, dose_steps = dose_steps, alpha = alpha, beta = beta)
         physical_doses[index] = physical_dose
     total_penalty = ((physical_doses - min_dose) * volumes[-number_of_fractions:]).sum()
     return physical_doses, accumulated_doses, total_penalty

app.py
@@ -76,24 +76,13 @@ if st.button('compute optimal dose', help = 'takes the given inputs from above t
             st.pyplot(af.actual_policy_plotter(predicted_policies,volumes_to_check))   
     else:
         [physical_doses, accumulated_doses, total_penalty] = af.adaptfx_full(volumes = np.array(overlaps), number_of_fractions = int(fractions), min_dose = float(minimum_dose), max_dose = float(maximum_dose), mean_dose = float(mean_dose), dose_steps = float(dose_steps))
-        col1, col2, col3, col4, col5 = st.columns(5)  
-        with col1:
-            st.metric(label="**overlap**", value = str(overlaps[-5]) + 'cc')
-            st.metric(label="**first fraction**", value = str(physical_doses[0]) + 'Gy', delta = (physical_doses[0] - float(mean_dose)))
-        with col2:
-            st.metric(label="**overlap**", value = str(overlaps[-4]) + 'cc')
-            st.metric(label="**second fraction**", value = str(physical_doses[1]) + 'Gy', delta = (physical_doses[1] - float(mean_dose)))
-        with col3:
-            st.metric(label="**overlap**", value = str(overlaps[-3]) + 'cc')
-            st.metric(label="**third fraction**", value = str(physical_doses[2]) + 'Gy', delta = (physical_doses[2] - float(mean_dose)))
-        with col4:
-            st.metric(label="**overlap**", value = str(overlaps[-2]) + 'cc')
-            st.metric(label="**fourth fraction**", value = str(physical_doses[3]) + 'Gy', delta = (physical_doses[3] - float(mean_dose)))
-        with col5:
-            st.metric(label="**overlap**", value = str(overlaps[-1]) + 'cc')
-            st.metric(label="**fifth fraction**", value = str(physical_doses[4]) + 'Gy', delta = (physical_doses[4] - float(mean_dose)))
+        cols = st.columns(int(fractions))
+        for i, col in enumerate(cols):
+            with col:
+                st.metric(label=f"**overlap**", value=f"{overlaps[-(int(fractions) - i)]}cc")
+                st.metric(label=f"**fraction {i + 1}**", value=f"{physical_doses[i]}Gy", delta=(physical_doses[i] - float(mean_dose)))
         st.header('Plan summary')
         st.markdown('The adaptive plan achieved a total penalty of:')
-        st.metric(label = "penalty", value = str(total_penalty) + 'ccGy', delta = np.round(total_penalty - (np.array(overlaps[-5:])*(float(mean_dose)- float(minimum_dose))).sum(),2), delta_color= 'inverse')
+        st.metric(label = "penalty", value = str(total_penalty) + 'ccGy', delta = np.round(total_penalty - (np.array(overlaps[-int(fractions):])*(float(mean_dose)- float(minimum_dose))).sum(),2), delta_color= 'inverse')
         st.markdown('The arrow shows the comparison to standard fractionation, i.e. (number of fractions x mean dose). A green arrow shows an improvement.')
 